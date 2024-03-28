4db90fd0ba6e9535e8673ae35b652493e7e711af
farazkhan2062 <farazkhan2062@gmail.com>
2024-03-28 19:54:35 +0500

    ngs variable change

--- ngs.js
+++ ngs.js
@@ -1,3 +1,5 @@
 "use strict";
 let ngs = "Nurture Grammar School";
 console.log(`I Study in ${ngs}`);
+ngs = "my home town";
+console.log(`I study in ${ngs}`);
--- ngs.ts
+++ ngs.ts
@@ -2,7 +2,9 @@
 let ngs = "Nurture Grammar School";
 console.log(`I Study in ${ngs}`);
 
+ngs = "my home town"
 
+console.log(`I study in ${ngs}`);
 
 
 
